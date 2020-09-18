99382250f79ad5d9d283e03de5165ec3eccfb468
takumi1995 <70883657+takumi1995@users.noreply.github.com>
2020-09-18 18:27:44 +0900

    課題提出テスト
    データ型・文字列テスト

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,39 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+          int hogeInt = 50;
+
+          System.out.println("int型 = hogeInt = " + hogeInt);
+
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化 
+          String hogeString = "テスト";
+          
+          System.out.println(hogeString);
+
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [double型のhogeDoubleという変数を扱い、初期値は0.2だよ]
+        double hogeDouble = 0.2;
+
+        // ④変数 hogeInt を値「100」で上書き
+          hogeInt = 100;
+
+
+        // ⑤変数 hogeString を値「合格」で上書き
+          hogeString = "合格";
+
+
+        // ⑥変数 hogeInt の値を表示 
+         System.out.println(hogeInt);
+
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [変数のhogeStringの値を表示している]
+        System.out.println(hogeString);
+
+        // ⑧変数 hogeDouble の値を表示
+         System.out.println(hogeDouble);
+
+
+    }
+}
